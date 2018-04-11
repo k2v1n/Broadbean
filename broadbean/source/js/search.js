59b6667b6eed29235c8199ad5f05972309b019e4
Aaron-Bird <25508292+Aaron-Bird@users.noreply.github.com>
2018-04-12 01:35:14 +0800

    fix meta,change css

--- broadbean/source/js/search.js
+++ broadbean/source/js/search.js
@@ -83,7 +83,7 @@ var searchFunc = function(path, search_id, content_id) {
                     if (isMatch) {
                         // fix relative link bug
                         // str += "<li><a href='"+ data_url +"' class='search-result-title'>"+ data_title +"</a>";
-                        str += "<li><a href='/"+ data_url +"' class='search-result-title'>"+ data_title +"</a>";
+                        str += "<li><a href='"+ data_url +"' class='search-result-title'>"+ data_title +"</a>";
                         var content = data.content.trim().replace(/<[^>]+>/g,"");
                         if (first_occur >= 0) {
                             // cut out 100 characters
